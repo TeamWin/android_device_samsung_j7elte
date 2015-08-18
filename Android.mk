02356f9d4933cfadb91d347d3612f2ec65425ae0
Ethan Yonker <dees_troy@teamw.in>
2015-08-18 14:51:21 -0500

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,3 @@
+ifneq ($(filter j7elte,$(TARGET_DEVICE)),)
+    include $(all-subdir-makefiles)
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk \
+    $(LOCAL_DIR)/omni_j7elte.mk
