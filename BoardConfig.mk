7e6d6766d55b2add2b59750e9f94c4cbd3ca880b
Stricted <info@stricted.net>
2018-09-08 21:55:43 +0200

    j7elte: build dtb from source
    Change-Id: I1b93750c8b8e9854eef02353d0783012af054e9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,5 @@ include device/samsung/exynos7580-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := lineageos_j7elte_defconfig
 
-BOARD_MKBOOTIMG_ARGS += --dt device/samsung/j7elte/prebuilt/dt.img
-
-#BOARD_KERNEL_SEPARATED_DT := true
-#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
